6c1b0f6cc1adfee2bcc8c9a6c594e18d186a3e26
Gaurav Sarage <gauravsarage.1902@gmail.com>
2024-02-05 22:28:18 +0530

    Added `blog.ts` schema for blog articles in `schemas` folder

---
+++ sanity/schemas/blog.ts
@@ -0,0 +1,37 @@
+export default {
+    name: 'blog',
+    type: 'document',
+    title: 'Blog',
+    fields: [
+        {
+            name: 'title',
+            type: 'string',
+            title: 'Title of Blog Article',
+        },
+        {
+            name: 'slug',
+            type: 'slug',
+            title: 'Slug of your blog article',
+        },
+        {
+            name: 'titleImage',
+            type: 'image',
+            title: 'Title Image',
+        },
+        {
+            name: 'smallDescription',
+            type: 'text',
+            title: 'Small Description',
+        },
+        {
+            name: 'content',
+            type: 'array',
+            title: 'Content',
+            of: [
+                {
+                    type: 'block'
+                },
+            ],
+        },
+    ],
+}
