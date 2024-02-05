e35d19fc39bbf5561ecdfc22e99963dfe903e152
Gaurav Sarage <gauravsarage.1902@gmail.com>
2024-02-05 23:39:00 +0530

    Add Next.js Link component to Navbar for homepage navigation

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function Navbar() {
+    return (
+        <nav className="w-full relative flex items-center justify-between max-w-2xl mx-auto px-4 py-5">
+            <Link href="/" className="font-bold text-3xl">
+                Dev<span className="text-blue-500">Dazzle</span>
+            </Link>
+        </nav>
+    )
+}
